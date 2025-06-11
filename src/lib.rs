f8943c70f93f75cb2f9e23eb7cfc2bde2e1a7659
Juan Murua Olalde <juan@susosise.es>
2025-06-11 17:15:37 +0200

    Temporalmente, desactivar algunos warnings
    En algunas ocasiones puede resultar práctico reducir el  "ruido" del linter avisando de partes sin utilizar aún.
    Aunque, obviamente, no hay que abusar.... (El linter es tu amigo;,pero si lo apagas, no puede ayudarte ;-)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// TODO, acordarme de comentar estos dos allow cuando el código esté más maduro
+#![allow(unused_variables)]
+//#![allow(dead_code)]
+
 pub mod estancias_y_reservas;
 pub mod habitaciones;
 pub mod huespedes;
